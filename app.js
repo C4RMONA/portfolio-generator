e3cccca10cb08d18eb712569432576abe0866e86
chris <christiancarmona3@gmail.com>
2022-06-26 20:27:18 -0500

    updated feature/questions

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
 
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+  ])
+  .then(answer => console.log(answer));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
